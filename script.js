08bb877587b3f249871afc68dfcefcea08410b36
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-30 12:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function palindrome(string){
     const len = string.length;
 
     // loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
+    for (let i = 0; i <= len / 2; i++) {
 
         // check if first and last string are same
         if (string[i] !== string[len - 1 - i]) {
